3b433c95011527b48da6ec6ec736eac83312ad6e
Yashraj Vaghela <yashraj.vaghela@growexx.com>
2026-02-05 15:42:21 +0530

    Initial product class

---
+++ product.js
@@ -0,0 +1,12 @@
+class Product {
+constructor(name, price) {
+this.name = name;
+this.price = price;
+this.discount = 0;
+}
+getPrice() {
+return this.price;
+}
+}
+module.exports = Product;
+4. Commit with message "Initial pro
