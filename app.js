f61d1f08dd170d277109cf534dec2443c634cee0
Yashraj Vaghela <yashraj.vaghela@growexx.com>
2026-02-05 16:29:52 +0530

    add server setup configuration

--- app.js
+++ app.js
@@ -2,5 +2,8 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
 res.send('Welcome to the App');
+});const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`);
 });
 module.exports = app;
