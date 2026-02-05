d83fd76d3b929abd7b0d9c4fc62cddae98dc6af4
Yashraj Vaghela <yashraj.vaghela@growexx.com>
2026-02-05 15:57:08 +0530

    feat: add password validation helper
    typo fix

--- auth.js
+++ auth.js
@@ -1,6 +1,10 @@
 function login(username,password) {
 // TODO
+//  VALIDATE CREDENTIALS
 }
 function logout() {
 console.log("Logging out");
+}
+function validatePassword(password) {
+return password.length >= 8;
 }
